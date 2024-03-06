620420b4bf6e61529c1c347d0146a95c2914fd75
Maxim Petrov <mail@maximpetrov.net>
2024-03-07 01:39:37 +0400

    Update defining an index

---
+++ tasks/yandex/2_defining_an_index_for_insertion/main.py
@@ -0,0 +1,28 @@
+from decorators import ExecutionTimeDecorator
+from decorators import TestDecorator
+from decorators import MemoryProfilerDecorator
+
+execution_time_decorator = ExecutionTimeDecorator
+test_decorator = TestDecorator
+memory_profiler_decorator = MemoryProfilerDecorator
+
+
+# @memory_profiler_decorator
+# @execution_time_decorator(num_runs=100)
+# @test_decorator
+def main():
+    with open('input.txt', 'r') as file_in:
+        numbers = tuple(
+            int(number) for number in next(file_in).strip().split(' ')
+        )
+        target = int(next(file_in))
+
+        indexed_numbers = enumerate(numbers)
+        for index, number in indexed_numbers:
+            if target <= number:
+                print(index)
+                break
+
+
+if __name__ == '__main__':
+    main()
