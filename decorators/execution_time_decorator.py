fb408e9adc857ca36df604fcde7b9ee026e7d1b5
Maxim Petrov <mail@maximpetrov.net>
2024-03-07 00:10:19 +0400

    Fix execution_time_decorator

--- decorators/execution_time_decorator.py
+++ decorators/execution_time_decorator.py
@@ -1,22 +1,24 @@
 import time
+from functools import wraps
 
 
 class ExecutionTimeDecorator:
-    def __init__(self, func, num_runs=100):
-        self.func = func
+    def __init__(self, num_runs=100):
         self.num_runs = num_runs
-        self.__name__ = func.__name__
 
-    def __call__(self, *args, **kwargs):
-        total_time = 0
-        for _ in range(self.num_runs):
-            start_time = time.time()
-            result = self.func(*args, **kwargs)
-            end_time = time.time()
-            total_time += (end_time - start_time)
-        average_time = total_time / self.num_runs
-        print(
-            f"Average execution time over {self.num_runs} "
-            f"runs: {average_time:.6f} seconds."
-        )
-        return result
+    def __call__(self, func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            total_time = 0
+            for _ in range(self.num_runs):
+                start_time = time.time()
+                result = func(*args, **kwargs)
+                end_time = time.time()
+                total_time += (end_time - start_time)
+            average_time = total_time / self.num_runs
+            print(
+                f"Average execution time over {self.num_runs} "
+                f"runs: {average_time:.6f} seconds."
+            )
+            return result
+        return wrapper
