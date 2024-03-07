2155adacda0dc0923aa8ad383960aa1006ebcaf3
Maxim Petrov <mail@maximpetrov.net>
2024-03-07 12:36:17 +0400

    2_defining_an_index_for_insertion: small update

--- tasks/yandex/2_defining_an_index_for_insertion/main_2.py
+++ tasks/yandex/2_defining_an_index_for_insertion/main_2.py
@@ -8,11 +8,11 @@ test_decorator = TestDecorator
 memory_profiler_decorator = MemoryProfilerDecorator
 
 
-# @memory_profiler_decorator
-# @execution_time_decorator(num_runs=1)
-# @test_decorator
+@memory_profiler_decorator
+@execution_time_decorator(num_runs=1)
+@test_decorator
 def main():
-    with open('input_2.txt', 'r') as file_in:
+    with open('input.txt', 'r') as file_in:
         numbers = tuple(
             int(number) for number in next(file_in).strip().split(' ')
         )
