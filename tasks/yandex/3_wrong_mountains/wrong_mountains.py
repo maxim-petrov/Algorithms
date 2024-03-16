1cf26bbe89952db5d860245bcf8f1431f70d7199
Maxim Petrov <mail@maximpetrov.net>
2024-03-16 13:13:44 +0400

    Wrong mountain  - fix

--- tasks/yandex/3_wrong_mountains/wrong_mountains.py
+++ tasks/yandex/3_wrong_mountains/wrong_mountains.py
@@ -7,44 +7,48 @@ test_decorator = TestDecorator
 memory_profiler_decorator = MemoryProfilerDecorator
 
 
-@memory_profiler_decorator
-@execution_time_decorator(num_runs=1)
-@test_decorator
-def main(input_filename):
-    with open(input_filename, 'r') as file_in:
+def read_numbers_from_file(filename):
+    with open(filename, 'r') as file_in:
         numbers = tuple(
-            int(number) for number in next(file_in).strip().split(' ')
+            int(number) for number in next(file_in).strip().split()
         )
-        numbers_length = len(numbers) - 1
-
-        if numbers_length >= 2:
-            result = check_if_mountain_sequence(numbers, numbers_length)
-        else:
-            result = False
-
-        print(result)
+    return numbers
 
 
-def check_if_mountain_sequence(numbers, numbers_length):
-    is_peak_reached = False
-    for number_index in range(0, numbers_length):
+def is_mountain_sequence(numbers):
+    numbers_length = len(numbers)
+
+    if numbers_length < 3:
+        return False
+
+    is_peak_found = False
+    for number_index in range(0, numbers_length - 1):
         current_number = numbers[number_index]
         next_number = numbers[number_index + 1]
 
         if current_number == next_number:
             return False
-        elif not is_peak_reached:
-            if current_number > next_number:
-                if number_index == 0:
-                    return False
-                is_peak_reached = True
-        else:
-            if current_number < next_number:
+        elif current_number > next_number:
+            if not number_index:
                 return False
-    if is_peak_reached:
-        return True
-    else:
-        return False
+            if not is_peak_found:
+                is_peak_found = True
+        elif is_peak_found:
+            return False
+
+    return is_peak_found
+
+
+@memory_profiler_decorator
+@execution_time_decorator(num_runs=1)
+@test_decorator
+def main(input_filename):
+    try:
+        numbers = read_numbers_from_file(input_filename)
+        result = is_mountain_sequence(numbers)
+        print(result)
+    except IOError:
+        print("File not found or unable to read")
 
 
 if __name__ == '__main__':
