c2dd9595bd3980af38a0d36223da2208d7e63b2f
Maxim Petrov <mail@maximpetrov.net>
2024-03-19 11:22:50 +0400

    Add changes

---
+++ tasks/yandex/9_fibonacci_barometer/fibonacci_barometer.py
@@ -0,0 +1,54 @@
+from decorators import ExecutionTimeDecorator
+from decorators import TestDecorator
+from decorators import MemoryProfilerDecorator
+
+execution_time_decorator = ExecutionTimeDecorator
+test_decorator = TestDecorator
+memory_profiler_decorator = MemoryProfilerDecorator
+
+
+def read_data(filename: str, num_lines: int = 1):
+    """Reads a specified number of lines from a file."""
+    lines = []
+    with open(filename, 'r') as file_in:
+        try:
+            for i in range(num_lines):
+                lines.append(
+                    next(file_in).strip()
+                )
+            return lines
+        except StopIteration:
+            return None
+
+
+@memory_profiler_decorator
+@execution_time_decorator(num_runs=1)
+@test_decorator
+def main(input_filename: str):
+    """Calculate and print the number of platforms needed for robots based on
+    their weights and a weight limit.
+    """
+    try:
+        data = read_data(input_filename, 2)
+        robots_weights = sorted([int(weight) for weight in data[0].split()])
+        weight_limit: int = int(data[1])
+
+        platform_count: int = 0
+        left: int = 0
+        right: int = len(robots_weights) - 1
+
+        while left <= right:
+            total_weight: int = robots_weights[left] + robots_weights[right]
+
+            if total_weight <= weight_limit:
+                left += 1
+            right -= 1
+            platform_count += 1
+
+        print(platform_count)
+    except IOError:
+        print("File not found or unable to read")
+
+
+if __name__ == '__main__':
+    main(input_filename='input.txt')
