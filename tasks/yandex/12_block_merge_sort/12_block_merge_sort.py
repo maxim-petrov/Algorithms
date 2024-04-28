aa56191e326cbd45545173f3199b397716a6ea31
Maxim Petrov <mail@maximpetrov.net>
2024-04-28 18:01:14 +0400

    The first approach

---
+++ tasks/yandex/12_block_merge_sort/12_block_merge_sort.py
@@ -0,0 +1,53 @@
+# 3 2 0 1 4 6 5
+
+from itertools import islice
+
+
+def read_data(filename: str, num_lines: int = 2):
+    """Reads a specified number of lines from a file."""
+    with open(filename, 'r') as file_in:
+        try:
+            lines = [line.strip() for line in islice(file_in, num_lines)]
+            return lines
+        except StopIteration:
+            return None
+
+
+def process_data(data):
+    return (
+        int(data[0]),
+        [int(num) for num in data[1].split()]
+    )
+
+
+def block_merge_sort(num_count, nums_to_sort):
+    result = 0
+    subtractor = 0
+    maximum = 0
+    count = []
+    for i in range(0, num_count):
+        if len(count) - 1 < maximum:
+            count.append(nums_to_sort[i])
+            if nums_to_sort[i] - subtractor > maximum:
+                maximum = nums_to_sort[i] - subtractor
+        else:
+            result += 1
+            subtractor = maximum + 1
+            maximum = 0
+            count = []
+            if len(count) - 1 < maximum:
+                count.append(nums_to_sort[i])
+                if nums_to_sort[i] - subtractor > maximum:
+                    maximum = nums_to_sort[i] - subtractor
+    result += 1
+    print(result)
+
+
+def main(input_filename: str):
+    data = read_data(input_filename, 2)
+    num_count, nums_to_sort = process_data(data)
+    block_merge_sort(num_count, nums_to_sort)
+
+
+if __name__ == '__main__':
+    main(input_filename='input.txt')
