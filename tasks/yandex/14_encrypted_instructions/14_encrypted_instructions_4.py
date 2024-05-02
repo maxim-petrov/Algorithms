674d16ad6fb50cf8381e8578b02aa8169c522045
Maxim Petrov <mail@maximpetrov.net>
2024-05-02 21:56:27 +0400

    Encrypted instructions: Search for a solution 2

--- tasks/yandex/14_encrypted_instructions/14_encrypted_instructions_4.py
+++ tasks/yandex/14_encrypted_instructions/14_encrypted_instructions_4.py
@@ -2,28 +2,64 @@ data = '2[a]3[b]2[c]'
 
 arr = [
     {
-        'multiplier': 2,
-        'inner_value': [
-            {
-                'multiplier': 2,
-                'inner_value': 'a'
-            },
-            {
-                'multiplier': 2,
-                'inner_value': 'c'
-            },
-        ]
+        'multiplier': 1,
+        'inner_value': 'a'
     },
     {
         'multiplier': 3,
-        'inner_value': 'b'
+        'inner_value': 'ac'
     },
     {
         'multiplier': 2,
-        'inner_value': 'c'
+        'inner_value': [
+            {
+                'multiplier': 1,
+                'inner_value': 'bc'
+            },
+            {
+                'multiplier': 2,
+                'inner_value': 'a'
+            }
+        ]
     }
 ]
 
+
+def compile_arr(data):
+    arr = []
+
+    string = ''
+    multiplier = ''
+
+    left, right = 0, 0
+    for idx, char in enumerate(data):
+        if char.isdigit() and left == 0:
+            multiplier += char
+            continue
+        else:
+            string += char
+
+        if char == '[':
+            left += 1
+
+            if multiplier and left == 1:
+                multiplier = int(multiplier)
+            elif not multiplier and left == 1:
+                multiplier = 1
+        if char == ']':
+            right += 1
+
+            if left == 1 and right == 1:
+                arr.append({
+                    'multiplier': multiplier,
+                    'inner_value': string.strip('[]')
+                })
+                left = 0
+                right = 0
+                string = ''
+                multiplier = ''
+
+    return arr
 
 def process_data(arr):
     result = ''
@@ -35,4 +71,6 @@ def process_data(arr):
     return result
 
 
-print(process_data(arr))
+arr_2 = compile_arr(data)
+print(arr_2)
+print(process_data(arr_2))
